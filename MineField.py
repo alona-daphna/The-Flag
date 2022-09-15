c5d175d546f399f37c5acaba937418013273673d
alona-daphna <alona.daphna@gmail.com>
2022-09-15 14:17:21 +0300

    define functions

---
+++ MineField.py
@@ -0,0 +1,10 @@
+def initialize_matrix():
+    pass
+
+# returns a list of tuples [(row, col)]
+def locate_flag():
+    pass
+
+# returns a list of tuples [(row, col)]
+def locate_bombs():
+    pass
---
+++ Soldier.py
@@ -0,0 +1,15 @@
+# initializes soldier's location at the start of the game
+def place_soldier():
+    pass
+
+# returns a tuple (row, col)
+def get_location_legs():
+    pass
+
+# returns a tuple (row, col)
+def get_location_body():
+    pass
+
+# checks that the soldier is not out of game's bounds
+def is_not_out_of_range():
+    pass
