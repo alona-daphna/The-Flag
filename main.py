7972414f45a88f6492023fba2d34f1d7de8d7c1d
alona-daphna <alona.daphna@gmail.com>
2022-09-18 11:06:39 +0300

    fix events in main

--- main.py
+++ main.py
@@ -8,26 +8,33 @@ import consts
 
 # initialize matrix:
 
-
+pygame.init()
 game_running = True
 
 matrix = MineField.initialize_matrix()
 for r in range(len(matrix)):
     print(matrix[r])
-while running:
-    events = pygame.event
+while game_running:
+    events = pygame.event.get()
     for event in events:
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.KEY_UP:
-                Soldier.update_soldier_location(consts.UP)
+            if event.key == pygame.K_UP:
+                print('UP')
+                # Soldier.update_soldier_location(consts.UP)
             
-            if event.key == pygame.KEY_DOWN:
-                Soldier.update_soldier_location(consts.DOWN)
+            if event.key == pygame.K_DOWN:
+                print('DOWN')
+                # Soldier.update_soldier_location(consts.DOWN)
             
-            if event.key == pygame.KEY_LEFT:
-                Soldier.update_soldier_location(consts.LEFT)
+            if event.key == pygame.K_LEFT:
+                print('LEFT')
+                # Soldier.update_soldier_location(consts.LEFT)
             
-            if event.key == pygame.KEY_RIGHT:
-                Soldier.update_soldier_location(consts.RIGHT)
+            if event.key == pygame.K_RIGHT:
+                print('RIGHT')
+                # Soldier.update_soldier_location(consts.RIGHT)
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            break
 
 
