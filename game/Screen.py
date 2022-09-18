2b12e685ebd7e063a55ce221a8bc39dd46788de5
Noa Hatuel <hatuelnoa@gmail.com>
2022-09-18 14:32:44 +0300

    Added grid screen

--- game/Screen.py
+++ game/Screen.py
@@ -2,28 +2,72 @@ import pygame
 import consts
 import random
 import MineField
+import time
 
 screen = pygame.display.set_mode(
         (consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
 
 
-def draw_game():
+def draw_game(matrix):
+    coordinates = random_coordinates()
+    draw_grass(coordinates)
+    time.sleep(3)
+    present_bomb_screen()
+    draw_grass(coordinates)
+    time.sleep(3)
+
+
+def present_bomb_screen():
+    screen.fill(consts.BLACK)
+    draw_grid()
+    draw_bombs(MineField.get_bomb_locations())
+    time.sleep(5)
+
+
+def draw_grid():
+    for row in range(0, consts.WINDOW_WIDTH, consts.HEIGHT_MULTIPLIER):
+        for col in range(0, consts.WINDOW_HEIGHT, consts.WIDTH_MULTIPLIER):
+            rect = pygame.Rect(row, col, 20, 20)
+            pygame.draw.rect(screen, consts.GREEN, rect, 1)
+
+
+def draw_bombs(bomb_coordinates):
+    myimage = pygame.image.load("bomb.png")
+    myimage = pygame.transform.scale(myimage,
+                                     (consts.BOMB_WIDTH_PIXEL,
+                                      consts.BOMB_HEIGHT_PIXEL))
+    draw_objects(bomb_coordinates, myimage)
+
+
+def draw_grass(grass_coordinates):
     screen.fill(consts.BACKGROUND_COLOR)
-    grass_index = draw_grass(random_index())
+    myimage = pygame.image.load("grass.png")
+    myimage = pygame.transform.scale(myimage,
+                                     (consts.GRASS_WIDTH, consts.GRASS_HEIGHT))
+    draw_objects(grass_coordinates, myimage)
+
+
+def draw_objects(coordinates, image):
+    for coordinate in coordinates:
+        screen.blit(image, coordinate)
     pygame.display.flip()
 
 
-def draw_grass(indexes):
-    myimage = pygame.image.load("grass.png")
-    myimage = pygame.transform.scale(myimage, (consts.GRASS_WIDTH, consts.GRASS_HEIGHT))
-    for item in indexes:
-        screen.blit(myimage, item)
+def get_coordinates_bomb(matrix):
+    coordinates = []
+    for row in range(consts.ROWS):
+        for col in range(consts.COLS):
+            if matrix[row][col] == consts.BOMB:
+                coordinates.append((row, col))
+    return coordinates
 
-def random_index():
+
+def random_coordinates():
     index_list = []
     while len(index_list) != consts.NUM_OF_OBJECTS:
         random_x = random.randint(0, consts.WINDOW_WIDTH - consts.GRASS_WIDTH)
-        random_y = random.randint(0, consts.WINDOW_HEIGHT - consts.GRASS_HEIGHT)
+        random_y = random.randint(0,
+                                  consts.WINDOW_HEIGHT - consts.GRASS_HEIGHT)
         index_tuple = tuple((random_x, random_y))
         if index_tuple not in index_list:
             index_list.append(index_tuple)
