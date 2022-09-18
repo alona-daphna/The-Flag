61fa6fd6ff85e80937d73df726bd56bf4d3383d6
alona-daphna <alona.daphna@gmail.com>
2022-09-18 10:37:34 +0300

    place bombs in matrix

--- MineField.py
+++ MineField.py
@@ -1,6 +1,7 @@
 import pygame
 import random
-import Screen
+import consts
+# import Screen
 
 matrix = []
 
@@ -20,20 +21,26 @@ def initialize_matrix():
 
 def random_index():
     random_row = random.randint(0, consts.ROWS-1)
-    random_col = random.randint(0, consts.COLS-1)
-    return (random_col, random_row)
+    random_col = random.randint(0, consts.COLS - consts.BOMB_WIDTH)
+    return (random_row, random_col)
 
 
 def place_flag():
-    pass
+    r_start, c_start = consts.FLAG_START_PLACE
+    for r in range(consts.ROWS-2, consts.ROWS - consts.FLAG_WIDTH - 2, -1):
+        for c in range(consts.COLS -1, consts.COLS - consts.FLAG_HEIGHT - 2, -1):
+            matrix[r][c] == consts.FLAG
 
 def place_soldier():
-    pass
+    r_start, c_start = consts.SOLDIER_START_PLACE
+    for r in range(consts.SOLDIER_HEIGHT):
+        for c in range(consts.SOLDIER_WIDTH):
+            matrix[r][c] = consts.SOLDIER
 
 def place_bombs():
     for i in range(consts.BOMB_COUNT):
         row, col = random_index()
-        if is_empty(matrix, row, col, consts.BOMB_WIDTH):
+        if is_empty(row, col, consts.BOMB_WIDTH):
             for j in range(consts.BOMB_WIDTH):
                 matrix[row][col+j] = consts.BOMB
                 
@@ -46,11 +53,14 @@ def is_empty(r, c, length):
     return True
 
 
-def update_matrix(Screen.screen):
+def update_matrix():
     pass
 
 
 def is_touching_bomb():
+    pass
+
+def is_touching_flag():
     pass
 
 def locate_object(obj):
