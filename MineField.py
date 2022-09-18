09af43e21d3b9f50a9e6e4c70f4a4400fdae6f9e
alona-daphna <alona.daphna@gmail.com>
2022-09-18 09:38:21 +0300

    initialize matrix

--- MineField.py
+++ MineField.py
@@ -1,15 +1,57 @@
+import pygame
+import random
+import Screen
+
+matrix = []
+
 def initialize_matrix():
+    for r in range(consts.ROWS):
+        row_to_add = []
+        for c in range(consts.COLS):
+            row_to_add.append(consts.EMPTY)
+        matrix.append(row_to_add)
+
+    place_flag()
+    place_soldier()
+    place_bombs()
+
+    return matrix
+
+
+def random_index():
+    random_row = random.randint(0, consts.ROWS-1)
+    random_col = random.randint(0, consts.COLS-1)
+    return (random_col, random_row)
+
+
+def place_flag():
     pass
 
-# returns a list of tuples [(row, col)]
-def locate_flag():
+def place_soldier():
     pass
 
-# returns a list of tuples [(row, col)]
-def locate_bombs():
+def place_bombs():
+    for i in range(consts.BOMB_COUNT):
+        row, col = random_index()
+        if is_empty(matrix, row, col, consts.BOMB_WIDTH):
+            for j in range(consts.BOMB_WIDTH):
+                matrix[row][col+j] = consts.BOMB
+                
+
+def is_empty(r, c, length):
+    for i in range(length):
+        if matrix[r][c+i] != consts.EMPTY:
+            return False
+    
+    return True
+
+
+def update_matrix(Screen.screen):
     pass
 
 
 def is_touching_bomb():
     pass
 
+def locate_object(obj):
+    pass
--- main.py
+++ main.py
@@ -4,7 +4,26 @@ import Soldier
 import MineField
 import consts
 
-# TODO: create game window
-# TODO: attend to user key interrupt
 # TODO: draw element to the screen
+
+# initialize matrix:
+
+
+game_running = True
+while running:
+    events = pygame.event
+    for event in events:
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.KEY_UP:
+                Soldier.update_soldier_location(consts.UP)
+            
+            if event.key == pygame.KEY_DOWN:
+                Soldier.update_soldier_location(consts.DOWN)
+            
+            if event.key == pygame.KEY_LEFT:
+                Soldier.update_soldier_location(consts.LEFT)
+            
+            if event.key == pygame.KEY_RIGHT:
+                Soldier.update_soldier_location(consts.RIGHT)
+
 
