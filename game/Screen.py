6378220a13e74c2b2de403410ffa795bee76fb95
Noa Hatuel <hatuelnoa@gmail.com>
2022-09-18 10:12:03 +0300

    Added grass to screen

--- game/Screen.py
+++ game/Screen.py
@@ -1,23 +1,30 @@
 import pygame
 import consts
 import random
+import MineField
 
 screen = pygame.display.set_mode(
         (consts.WINDOW_WIDTH, consts.WINDOW_HEIGHT))
 
+
 def draw_game():
     screen.fill(consts.BACKGROUND_COLOR)
-    draw_grass()
+    grass_index = draw_grass(random_index())
     pygame.display.flip()
 
 
-
-def draw_grass():
-    print(random_index())
+def draw_grass(indexes):
+    myimage = pygame.image.load("grass.png")
+    myimage = pygame.transform.scale(myimage, (consts.GRASS_WIDTH, consts.GRASS_HEIGHT))
+    for item in indexes:
+        screen.blit(myimage, item)
 
 def random_index():
-    random_x = random.randint(0, consts.WINDOW_WIDTH)
-    random_y = random.randint(0, consts.WINDOW_HEIGHT)
-    return tuple((random_x, random_y))
-
-
+    index_list = []
+    while len(index_list) != consts.NUM_OF_OBJECTS:
+        random_x = random.randint(0, consts.WINDOW_WIDTH - consts.GRASS_WIDTH)
+        random_y = random.randint(0, consts.WINDOW_HEIGHT - consts.GRASS_HEIGHT)
+        index_tuple = tuple((random_x, random_y))
+        if index_tuple not in index_list:
+            index_list.append(index_tuple)
+    return index_list
